0201f9ee36874a6be02d6a5a03894a65048e83a2
Maria Mejia <isazambra924o@gmail.com>
2026-03-26 17:49:01 -0600

    fix(db): corrección en migración, se duplicaba la creacion de tabla  registroPreclinico

--- backend/prisma/migrations/20260314040500_agregar_consulta_medica/migration.sql
+++ backend/prisma/migrations/20260314040500_agregar_consulta_medica/migration.sql
@@ -3,22 +3,6 @@ BEGIN TRY
 BEGIN TRAN;
 
 -- CreateTable
-CREATE TABLE [dbo].[registroPreclinico] (
-    [id] INT NOT NULL IDENTITY(1,1),
-    [expedienteId] INT NOT NULL,
-    [enfermeroId] INT NOT NULL,
-    [presionArterial] NVARCHAR(1000),
-    [temperatura] FLOAT(53),
-    [peso] FLOAT(53),
-    [frecuenciaCardiaca] INT,
-    [observaciones] TEXT,
-    [fechaRegistro] DATETIME2 NOT NULL CONSTRAINT [registroPreclinico_fechaRegistro_df] DEFAULT CURRENT_TIMESTAMP,
-    [createdAt] DATETIME2 NOT NULL CONSTRAINT [registroPreclinico_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
-    [updatedAt] DATETIME2 NOT NULL,
-    CONSTRAINT [registroPreclinico_pkey] PRIMARY KEY CLUSTERED ([id])
-);
-
--- CreateTable
 CREATE TABLE [dbo].[ConsultaMedica] (
     [id] INT NOT NULL IDENTITY(1,1),
     [expedienteId] INT NOT NULL,
@@ -45,11 +29,6 @@ CREATE TABLE [dbo].[RecetaMedica] (
     CONSTRAINT [RecetaMedica_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
--- AddForeignKey
-ALTER TABLE [dbo].[registroPreclinico] ADD CONSTRAINT [registroPreclinico_expedienteId_fkey] FOREIGN KEY ([expedienteId]) REFERENCES [dbo].[Expediente]([idExpediente]) ON DELETE NO ACTION ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE [dbo].[registroPreclinico] ADD CONSTRAINT [registroPreclinico_enfermeroId_fkey] FOREIGN KEY ([enfermeroId]) REFERENCES [dbo].[Usuario]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE [dbo].[ConsultaMedica] ADD CONSTRAINT [ConsultaMedica_expedienteId_fkey] FOREIGN KEY ([expedienteId]) REFERENCES [dbo].[Expediente]([idExpediente]) ON DELETE NO ACTION ON UPDATE CASCADE;
